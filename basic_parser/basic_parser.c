069746968de22b195e057013ed006c40071b26a9
johngable <jg4863@cs.ship.edu>
2021-03-17 10:48:46 -0400

    Somewhat working  list implemenatation parser.

--- basic_parser/basic_parser.c
+++ basic_parser/basic_parser.c
@@ -8,12 +8,14 @@ enum State {
     WHITESPACE, CHARACTER 
 }; 
 
+
 //Basic sentence struct to hold our sentence counts and pointer array
 struct sentence_struct
 {
     int sentence_count;
     char **sentence_array;
 };
+
 
 /**
  * Main struct for the linked list.
@@ -35,6 +37,8 @@ void displayList(struct list_head todo_list){
     struct list_head *curr; //Tracks current node during traversal
     struct argument *entry; //Current nodes struct with contents
 
+    
+
     for (curr = todo_list.next; curr->next != start; curr = curr->next) {
         entry = list_entry(curr, struct argument, list);
         printf("%s\n", entry->contents);
@@ -51,32 +55,50 @@ void stringExtract(struct list_head list_args, char *input, int length){
     int word_count = 0;
     int currentState = CHARACTER;
 
-    struct sentence_struct s_struct;
-    char *temp = calloc(50, sizeof(char));
-    //s_struct.sentence_array = calloc(10, sizeof(char*));
-    struct argument *arg = malloc(sizeof(struct argument));
+    char *temp = calloc(50, sizeof(char));  //temporary word variable
+    struct argument *arg;
+
     int tempLocation = 0;
 
     for(int i=0; i<length; i++){
+
+        //if we find characters to a word, add them to a temp variable
         if (input[i] != ' ' && input[i] != '"'){
             currentState = CHARACTER;
             temp[tempLocation] = input[i];
             tempLocation++;
-        }else if (input[i] == ' ' || input[i] == '\t'){
-            if(currentState!=WHITESPACE){
-                //s_struct.sentence_array[word_count] = calloc(strlen(temp)+1, sizeof(char));
-                //strncpy(s_struct.sentence_array[word_count], temp, 15);
-                //printf("%s", s_struct.sentence_array[word_count]);
+
+            //if we found the last word, and it has no space after
+            //add to the list
+            if(i == length-1){
+                arg = malloc(sizeof(struct argument));
                 arg->contents = strdup(temp);
                 list_add(&arg->list, &list_args);
-                currentState = WHITESPACE;
-                word_count++;
-                tempLocation=0;
                 memset(temp, 0, 50);
+            }
+
+        }else if (input[i] == ' ' || input[i] == '\t'){
+            if(currentState!=WHITESPACE){
+                currentState = WHITESPACE; 
+                
+                arg = malloc(sizeof(struct argument)); //allocate arg struct
+                arg->contents = strdup(temp); //store the last full word into contents of an argument
+                list_add(&arg->list, &list_args); //add the argument to the list of args
+               
+                word_count++;   //increment which word we are on
+                tempLocation=0; //reset character count for a word
+                memset(temp, 0, 50);    //reset the temp word variable to blank
             }
         
         }
     }
+
+    //The req specify ending the list of arguements with a NULL for exec
+    arg = malloc(sizeof(struct argument));
+    arg->contents = strdup("NULL");
+    list_add(&arg->list, &list_args);
+
+    displayList(list_args);
     
 }
 
@@ -88,7 +110,7 @@ int main(int argc, char **argv) {
 
     if(argc>0){
         stringExtract(list_args, argv[1], strlen(argv[1]));
-        displayList(list_args);
+        //displayList(list_args);
     }
 
 }
