d4aadc36fa15c6cdcc20f979076e4bf83b4d7f08
Hannah Moats <hannah13mo@DESKTOP-L32HF75.localdomain>
2021-03-19 09:32:05 -0400

    Added some functional decomp and comments to items added last night

--- basic_parser/basic_parser.c
+++ basic_parser/basic_parser.c
@@ -174,6 +174,12 @@ void makeArgumentList(struct list_head *list, char **args, int len) {
     //args = {"element1", "element2", NULL}; 
 }
 
+/**
+ * @brief Frees the memory contents created to store the argument list 
+ * 
+ * @param args The malloced array of char pointers being freed
+ * @param len The length of the list being freed 
+ */
 void free_exec_arg_list(char **args, int len) {
     int i; 
     for (i = 0; i < len; i++) {
@@ -188,18 +194,37 @@ void free_exec_arg_list(char **args, int len) {
  * be easier to test with the basic parser.
  * basic_executor.c 
  * **************************************************************************/
+
+/**
+ * @brief Handles the execution of the child process 
+ * 
+ * @param command The type of command being executed 
+ * @param args The list of args sent to exec 
+ */
 void handleChildInExecutor(char *command, char *const *args) {
     execvp(command, args); 
     perror("The process failed to execute"); //Should we handle an error here if we are unable to 
     exit(-1); 
 }
 
+/**
+ * @brief Handles the execution of the parent process 
+ * 
+ * @param pid The process id
+ * @param option The option passed to waitpid 
+ */
 void handleParentInExecutor(pid_t pid, int option) {
     int status; 
     waitpid(pid, &status, option); 
 }
 
-void executor(char *command, char *const *args) {
+/**
+ * @brief Executes the command, given the type of command and the argument array 
+ * 
+ * @param command The type of command that is being executed, Ex. /bin/ls
+ * @param args The array of args that exec() takes in
+ */
+void execute(char *command, char *const *args) {
     pid_t pid = fork(); 
 
     if (pid == 0) { //Child process 
@@ -207,6 +232,31 @@ void executor(char *command, char *const *args) {
     } else {  //Parent process 
         handleParentInExecutor(pid, 0); 
     }
+}
+
+/**
+ * @brief Runs the command typed on the command line 
+ * 
+ * @param len The length of the linked list 
+ * @param list_args The linked list of args that are being executed 
+ */
+void run_command(int len, struct list_head *list_args) {
+    //initializes an array of character pointers that will be passed to exec()
+    char **exec_arg_list = malloc(len * sizeof(char *)); 
+
+    //takes the linked list, and turns it into an array list that can be passed to exec
+    makeArgumentList(list_args, exec_arg_list, len); 
+    char *command = malloc((5 + strlen(exec_arg_list[0])) * sizeof(char));
+
+    // command becomes: /bin/<command> 
+    strcpy(command, "/bin/"); 
+    strcat(command, exec_arg_list[0]); 
+
+    //executes a basic command
+    execute(command, exec_arg_list); 
+
+    //frees the argument list
+    free_exec_arg_list(exec_arg_list, len); 
 }
 
 
@@ -222,26 +272,8 @@ int main(int argc, char **argv) {
     }
 
     //finds the length of the list, used to allocate space for the array of character pointers 
-    int list_len; 
-    char *command; 
-    list_len = getListLength(&list_args); 
-
-    //initializes an array of character pointers that will be passed to exec()
-    char **exec_arg_list = malloc(list_len * sizeof(char *)); 
-
-    //takes the linked list, and turns it into an array list that can be passed to exec
-    makeArgumentList(&list_args, exec_arg_list, list_len); 
-    command = malloc((5 + strlen(exec_arg_list[0])) * sizeof(char));
-
-    // command becomes: /bin/<command> 
-    strcpy(command, "/bin/"); 
-    strcat(command, exec_arg_list[0]); 
-
-    //executes a basic command
-    executor(command, exec_arg_list); 
-
-    //frees the argument list
-    free_exec_arg_list(exec_arg_list, list_len); 
+    int list_len = getListLength(&list_args); 
+    run_command(list_len, &list_args); 
 
     clear_list(&list_args); 
 
