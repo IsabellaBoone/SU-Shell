349652a9abf5e2dcf0a2625a7d16529a36071c69
Isabella Boone <ib2573@ship.edu>
2021-03-22 18:28:50 -0400

    Added internal command stuff

---
+++ project_code/internal.h
@@ -0,0 +1,6 @@
+static int handle_setenv(struct argument *arg); 
+static int handle_getenv(struct argument *arg); 
+static int handle_unsetenv(struct argument *arg); 
+static int handle_cd(struct argument *arg); 
+static int handle_pwd(struct argument *arg); 
+static int handle_exit(struct argument *arg); 
