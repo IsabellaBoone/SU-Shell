e5ba3b5e3bc9d22c07e82d7b3af1d0fceab7003d
Isabella Boone <ib2573@ship.edu>
2021-03-28 23:23:44 -0400

    Comments

--- project_code/environ.h
+++ project_code/environ.h
@@ -3,23 +3,31 @@
 
 #include "list.h"
 
+/**
+ * @brief An environment variable
+ * 
+ * @param name char* name of the environment variable (ex. NAME)
+ * @param contents char* the convents of envp[i]
+ * @param list_head list, part of a list 
+ * 
+ */
 struct environment {
-    char *name; //the name of the environment variable (ex. NAME)
-    char *contents; //the contents of the envp[i]
+    char *name; 
+    char *contents; 
     struct list_head list; 
 };
 
-void display_env_array(char **envp); 
+int set_env(struct list_head *list, char *name, char *value); 
+int unset_env(struct list_head *list, char *name); 
 void show_env(char **envp); 
 void display_env_list(struct list_head *list); 
+void display_env_array(char **envp); 
 char ** make_env_array(struct list_head *list); 
 void make_env_list(struct list_head *list, char **envp);  
 void free_env_list(struct list_head *list);
 void free_env_array(char **envp, int len); 
 char * get_env(struct list_head *list, char *name); 
 char * get_env_value(struct list_head *list, char *name);
-int set_env(struct list_head *list, char *name, char *value); 
-int unset_env(struct list_head *list, char *name); 
 void clear_list_env(struct list_head *list); 
 
 #endif
--- project_code/executor.c
+++ project_code/executor.c
@@ -17,21 +17,21 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h> // for memory allocation
-#include <stdio.h> // for expert debugging
+#include <stdio.h> // for input/output
 
 #include "executor.h"
 
 /**
  * @brief Handles the execution of the child process 
  * @author Hannah Moats 
- * @param command The type of command being executed 
- * @param args The list of args sent to exec 
+ * @param command char* Type of command being executed 
+ * @param args char* const The list of args sent to exec 
+ * @param env char** array of environment variables
  */
 void handleChildInExecutor(char *command, char *const *args, char **env) {
-    pid_t pid2 = execvpe(command, args, env); 
-
-    perror("The process failed to execute"); //Should we handle an error here if we are unable to 
-    exit(-1); 
+  pid_t pid2 = execvpe(command, args, env); // Execute command 
+  perror("The process failed to execute"); // Should only be reaches when error happens with execvpe
+  exit(-1); // Exit with error 
 }
 
 /**
@@ -41,36 +41,40 @@ void handleChildInExecutor(char *command, char *const *args, char **env) {
  * @param option The option passed to waitpid 
  */
 void handleParentInExecutor(pid_t pid, int option) {
-    int status; 
-    waitpid(pid, &status, option);
-    //printf("Child exited: %d\n", status);
+  int status; 
+  waitpid(pid, &status, option); // Wait for child to die
+  //printf("Child exited: %d\n", status);
 }
 
 /**
  * @brief Handles getting input from a file, outputting to a file for commands. 
- * @author Hannah Moats 
- * @author John Gable 
- * @author Isabella Boone
  *
  * @param subcmd The command who's input and output is being handled. 
  */
 static void handle_input_output(struct subcommand *subcmd) {
-    if (strcmp(subcmd->output, "stdout") != 0) {
-        const char *filename = subcmd->output; 
-        int fd = 0; 
-        if (subcmd->type == REDIRECT_OUTPUT_TRUNCATE) {
-            fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777); 
-        } else if (subcmd->type == REDIRECT_OUTPUT_APPEND) {
-            fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0777); 
-        }
-        close(STDOUT_FILENO); //closes stdout 
-        dup2(fd, STDOUT_FILENO); 
-    } if (strcmp(subcmd->input, "stdin") != 0) {
-        const char *filename = subcmd->input; 
-        int fd = open(filename, O_RDONLY);
-        close(STDIN_FILENO);  
-        dup2(fd, STDIN_FILENO); 
+  // If subcmd->output is anything other than stdout (default)
+  if (strcmp(subcmd->output, "stdout") != 0) {
+    const char *filename = subcmd->output; // Get filename
+    int fd = 0; // File descriptor 
+
+    // If type is truncate, open file with trucate flags
+    if (subcmd->type == REDIRECT_OUTPUT_TRUNCATE) {
+      fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777); 
+
+    // If type is append, open file with append flags
+    } else if (subcmd->type == REDIRECT_OUTPUT_APPEND) {
+      fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0777); 
     }
+    close(STDOUT_FILENO); // Close STDOUT
+    dup2(fd, STDOUT_FILENO); // Connect FD and STDOUT 
+
+  // If subcmd->input is anything other than "stdin" (default) 
+  } if (strcmp(subcmd->input, "stdin") != 0) {
+    const char *filename = subcmd->input; // Get filename
+    int fd = open(filename, O_RDONLY); // File descriptor, open file read only
+    close(STDIN_FILENO); // Close STDIN
+    dup2(fd, STDIN_FILENO); // Connect FD and STDIN
+  }
 }
 
 /**
@@ -80,15 +84,15 @@ static void handle_input_output(struct subcommand *subcmd) {
  * @param args The array of args that exec() takes in
  */
 void execute(char *command, char *const *args, struct subcommand *subcmd, char **env) {
-    pid_t pid = fork(); 
+  pid_t pid = fork(); 
 
-    if (pid == 0) { //Child process 
-        //if there is output
-        handle_input_output(subcmd); 
-        handleChildInExecutor(command, args, env); 
-    } else {  //Parent process 
-        handleParentInExecutor(pid, 0); 
-    }
+  if (pid == 0) { // Child process 
+    // if there is output
+    handle_input_output(subcmd); 
+    handleChildInExecutor(command, args, env); 
+  } else { // Parent process 
+    handleParentInExecutor(pid, 0); 
+  }
 
 }
 
@@ -106,80 +110,71 @@ void sig_handle(int signum)
 
 /**
  * @brief Runs the command typed on the command line including pipes
- * @author Hannah Moats 
- * @author John Gable (implemented pipes)
- * @author Isabella Boone
  * 
  * @param len The length of the linked listssss
  * @param list_args The linked list of args that are being executed 
  */
 void run_command(int subcommand_count, struct list_head *list_commands, char **env) {
-    struct subcommand *entry; 
+  struct subcommand *entry; 
+  struct list_head *curr;  
 
-    if(subcommand_count>=2){
-        // //initializes an array of character pointers that will be passed to exec()
-        // char **exec_arg_list = malloc(new_length * sizeof(char *)); 
-        
-        //Loops through each subcommand and executes
-        struct list_head *curr;  
-        int i=0;
-        int prev_output=0;  //we need this to hold the child output for our loop
-        int pipes[2];   //standard pipes
+  // If we have more than one subcommand
+  if(subcommand_count>=2){
+    int i=0; // Keep track of what subcommand we are on
+    int prev_output=0; // We need this to hold the child output for our loop
+    int pipes[2]; // Standard pipes
+    
+    // Initializes an array of character pointers that will be passed to exec()
+    // char **exec_arg_list = malloc(new_length * sizeof(char *)); 
+    
+    // Iterate through entire list until we reach the beginnign again. 
+    for (curr = list_commands->next; curr != list_commands; curr = curr->next) {
+      entry = list_entry(curr, struct subcommand, list); // Update entry to look at current subcommand
+      char *command = calloc((1 + strlen(entry->exec_args[0])), sizeof(char)); // Allcoate space for command
 
-        for (curr = list_commands->next; curr != list_commands; curr = curr->next) {
-            //Looks at one subcommand 
-            entry = list_entry(curr, struct subcommand, list); 
-            char *command = calloc((1 + strlen(entry->exec_args[0])),  sizeof(char));
+      // command becomes: /bin/<command> 
+      strcat(command, entry->exec_args[0]); 
 
-            // command becomes: /bin/<command> 
-            strcat(command, entry->exec_args[0]); 
+      // executes a basic command
+      int pipe_code = pipe(pipes);
 
-            // executes a basic command
-            
-            int pipe_code = pipe(pipes);
-            if(pipe_code < 0){
-                perror("Could not create pipes.\n");
-                return;
-            }
+      // If there was an error creating pipes
+      if(pipe_code < 0){
+        perror("Could not create pipes.\n");
+        return;
+      }
+    
+      pid_t pid = fork(); 
 
-            
-            pid_t pid = fork(); 
-            
-
-            if (pid == 0) { //Child process 
-                if(i<subcommand_count-1){   //All children except the last must write to parent
-                    dup2(prev_output, 0);   //read prev_output set by parent (initially empty)
-                    close(prev_output);     //close for safety
-                    dup2(pipes[1], 1);      //write output back to parent
-                    close(pipes[1]);        //close for safety
-                }else{  //The last child doesnt output to the parent for the loop 
-                    dup2(prev_output, 0);   //Read final pipe from parent
-                    close(prev_output);     //close for safety
-                }
-                
-                handle_input_output(entry);
-                handleChildInExecutor(command, entry->exec_args, env);
-                
-            } else {  //Parent process
-                prev_output=pipes[0];   //get output from the child, and ensure that we can save it for next child
-                signal(SIGCHLD, sig_handle);
-            }
-
-            i++;
-            free(command); 
+      if (pid == 0) { // Child process 
+        if(i<subcommand_count-1){ // All children except the last must write to parent
+          dup2(prev_output, 0); // read prev_output set by parent (initially empty)
+          close(prev_output); // close for safety
+          dup2(pipes[1], 1); // write output back to parent
+          close(pipes[1]); // close for safety
+        }else{ // The last child doesnt output to the parent for the loop 
+          dup2(prev_output, 0); // Read final pipe from parent
+          close(prev_output); // close for safety
         }
-    }else{
-        struct list_head *curr;  
-        for (curr = list_commands->next; curr != list_commands; curr = curr->next) {
-            //Looks at one subcommand 
-            entry = list_entry(curr, struct subcommand, list); 
-            char *command = calloc((1 + strlen(entry->exec_args[0])),  sizeof(char));
+                
+        handle_input_output(entry); 
+        handleChildInExecutor(command, entry->exec_args, env);
+                
+      } else { // Parent process
+        prev_output=pipes[0]; //get output from the child, and ensure that we can save it for next child
+        signal(SIGCHLD, sig_handle);
+      }
+      i++;
+      free(command); 
+    }
+  }else{ // Else, if we only have one command
+    for (curr = list_commands->next; curr != list_commands; curr = curr->next) {
+      entry = list_entry(curr, struct subcommand, list); // Update entry to look at current subcommand
+      char *command = calloc((1 + strlen(entry->exec_args[0])), sizeof(char));
 
-            // command becomes: /bin/<command> 
-            strcat(command, entry->exec_args[0]); 
-            execute(command, entry->exec_args, entry, env);
-            free(command); 
-        }
-
-   }
+      strcat(command, entry->exec_args[0]); // command becomes: /bin/<command> 
+      execute(command, entry->exec_args, entry, env); // Execute command
+      free(command); 
+    }
+  }
 }
